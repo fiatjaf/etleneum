68320f2090c4e8caad99fa479ccc37b7a950d452
fiatjaf <fiatjaf@gmail.com>
2019-10-17 19:56:06 +0000

    a little more client stuff.

---
+++ client/toast.js
@@ -0,0 +1,19 @@
+/** @format */
+
+import Toastify from 'toastify-js'
+
+function toast(type, html) {
+  Toastify({
+    text: html,
+    duration: 5000,
+    close: false,
+    gravity: 'top',
+    positionLeft: false,
+    className: `toast-${type}`
+  }).showToast()
+}
+
+export const info = html => toast('info', html)
+export const warning = html => toast('warning', html)
+export const error = html => toast('error', html)
+export const success = html => toast('success', html)
