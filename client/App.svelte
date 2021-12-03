e160f00fea03cc31c598837ecb959e9ea0ca5c00
fiatjaf <fiatjaf@gmail.com>
2021-12-03 11:12:40 -0300

    remove node badge.

--- client/App.svelte
+++ client/App.svelte
@@ -37,14 +37,6 @@
 <main><Router {routes} /></main>
 <footer>
   <span>Etleneum.com</span>
-  <a
-    href="https://ln.fiatjaf.com/node/02c16cca44562b590dd279c942200bdccfd4f990c3a69fad620c10ef2f8228eaff"
-  >
-    <img
-      alt="_"
-      src="https://img.shields.io/badge/dynamic/json?color=orange&label=node%2002bed%E2%80%A6&query=%24%5B0%5D.openchannels&suffix=%20channels&url=https%3A%2F%2Fln.bigsun.xyz%2Fapi%2Fnodes%3Fpubkey%3Deq.02c16cca44562b590dd279c942200bdccfd4f990c3a69fad620c10ef2f8228eaff26select%3Dopenchannels"
-    />
-  </a>
   <a href="/static/terms.txt">Terms</a>
 </footer>
 
